38b5e22e9d6c341840b4b883a9857d11a9efd1ac
Travis Harmon <twharmon@gmail.com>
2020-11-13 11:13:30 -0600

    Reliable directory file counting

--- bigfile.go
+++ bigfile.go
@@ -49,22 +49,27 @@ func (f *File) Close() error {
 
 // Size .
 func (f *File) Size() (int64, error) {
-	var stat syscall.Stat_t
-	if err := syscall.Stat(f.dir, &stat); err != nil {
-		if err == syscall.ENOENT {
+	fi, err := os.Open(f.dir)
+	if err != nil {
+		if os.IsNotExist(err) {
 			if err = os.MkdirAll(f.dir, dirPerm); err != nil {
 				return 0, fmt.Errorf("os.MkdirAll: %w", err)
 			}
 			return f.Size()
 		}
-		return 0, fmt.Errorf("syscall.Stat: %w", err)
+		return 0, fmt.Errorf("os.Open: %w", err)
 	}
-	fileCnt := (stat.Size - 64) / 32
+	names, err := fi.Readdirnames(-1)
+	fi.Close()
+	if err != nil {
+		return 0, fmt.Errorf("os.File.Readdirnames: %w", err)
+	}
+	fileCnt := int64(len(names))
 	if fileCnt == 0 {
 		return 0, nil
 	}
 	filledSize := int64(fileCnt-1) * f.partSize
-
+	var stat syscall.Stat_t
 	if err := syscall.Stat(filepath.Join(f.dir, padZeros(fileCnt-1)), &stat); err != nil {
 		return 0, fmt.Errorf("syscall.Stat: %w", err)
 	}
